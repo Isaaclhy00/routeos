86a01bb7827f417ea7cb72760812225782a7a9e8
Haoyang, Isaac Li <isaac.li@sembcorp.com>
2024-05-21 17:21:18 +0800

    Updated run file

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
 from flask import Flask, redirect, url_for
-# from flask_cors import CORS
 from routes import main
-# from optimization.services import services
 
 app = Flask(__name__)
 
 app.register_blueprint(main)
 
-# CORS(app)
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", debug=True, port=5000)
+    app.run()
